e8b65654caf1812573cf98faae531c1c187efd60
Muriel Damazio <murieldps@gmail.com>
2022-01-20 10:54:10 -0300

    Account and statement routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
+app.use(express.json());
+
+const customers = [];
+
+/*
+* cpf - string
+  name - string
+  id - uuid
+  statement []
+*/
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
+
+  customers.push({
+    name,
+    cpf,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer => customer.cpf === cpf))
+
+  return response.json(customer.statement)
 });
 
 app.listen(3333, console.log('App running at port: 3333'));
